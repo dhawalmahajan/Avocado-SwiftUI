8487ab0923c1c288c76ec1a6ef676fd9eb9c3691
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2020-02-02 08:45:17 +0530

    Initial Commit

---
+++ Avocado SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocado SwiftUI
+//
+//  Created by Dhawal Mahajan on 02/02/20.
+//  Copyright © 2020 Dhawal Mahajan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
